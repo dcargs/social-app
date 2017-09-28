3e51a25b7b864bb8627f5533ecce9e607f893650
Devin Cargill <devin@Devins-MacBook-Pro.local>
2017-09-28 15:47:56 -0500

    asdf

---
+++ index/home-page/statics/js/scripts.js
@@ -0,0 +1,11 @@
+$(function(){
+  $("#login", "#createAccount").hide()
+});
+
+function loginForm(){
+  $("#login").slideToggle();
+}
+
+function createAccountForm(){
+  $("#createAccount").slideToggle();
+}
--- index/home-page/views/landing.php
+++ index/home-page/views/landing.php
@@ -7,7 +7,12 @@
                 </div>
                 <hr />
 
-                <div class='row'>
+                <div class='row' id='choiceButtons'>
+                  <button type='button' class='btn btn-success btn-lg' onclick='loginForm()'>Login</button>
+                  <button type='button' class='btn btn-success btn-lg' onclick='createAccountForm()'>Create Account</button>
+                </div>
+
+                <div class='row' id='login'>
                   <div class='col-md-10'>
                     <form class='form-horizontal'>
                       <div class='form-group'>
@@ -31,12 +36,42 @@
                       </div>
                       <div class='form-group'>
                         <div class='col-sm-offset-2 col-sm-10'>
-                          <button type='submit' class='btn btn-default'>Submit</button>
+                          <button type='submit' class='btn btn-default'>Login</button>
                         </div>
                       </div>
                     </form>
                   </div>
-                  
+
+                  <div class='row' id='createAccount'>
+                    <div class='col-md-10'>
+                      <form class='form-horizontal'>
+                        <div class='form-group'>
+                          <label class='control-label col-sm-2' for='email'>Email:</label>
+                          <div class='col-sm-10'>
+                            <input type='email' class='form-control' id='email' placeholder='Enter email'>
+                          </div>
+                        </div>
+                        <div class='form-group'>
+                          <label class='control-label col-sm-2' for='pwd'>Password:</label>
+                          <div class='col-sm-10'>
+                            <input type='password' class='form-control' id='pwd' placeholder='Enter password'>
+                          </div>
+                        </div>
+                        <div class='form-group'>
+                          <div class='col-sm-offset-2 col-sm-10'>
+                            <div class='checkbox'>
+                              <label><input type='checkbox'> Remember me</label>
+                            </div>
+                          </div>
+                        </div>
+                        <div class='form-group'>
+                          <div class='col-sm-offset-2 col-sm-10'>
+                            <button type='submit' class='btn btn-default'>Create Account</button>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+
                 </div>
               </div>
               ";
