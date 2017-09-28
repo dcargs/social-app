a54b57995b3865e439fd6b44bd31820ed3ba5214
dcargs <cargilldevin@gmail.com>
2017-09-28 11:06:27 -0500

    changes

--- index/home-page/views/landing.php
+++ index/home-page/views/landing.php
@@ -1,7 +1,15 @@
 <?php
   $title = "Welcome Back";
   $onscreen_title = "Login";
-  $content = "<h3>Please login</h3>";
+  $content = "<div class='jumbotron'>
+                <div class='row'>
+                  <h2>Please login or create an account</h2>
+                </div>
+                <div class='row'>
+                  
+                </div>  
+              </div>
+              ";
 
   require "home-page/statics/layout.php";
  ?>
