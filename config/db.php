90c097b7f57ffbbdc3402c19502cf2b5f0daa168
Devin Cargill <devin@Devins-MacBook-Pro.local>
2017-09-28 16:49:09 -0500

    asdf

--- config/db.php
+++ config/db.php
@@ -1,5 +1,5 @@
 <?php
-  DEFINE("HOST", "ec2-34-210-88-30.us-west-2.compute.amazonaws.com");
+  DEFINE("HOST", "localhost");
   DEFINE("DBNAME", "social-app");
   DEFINE("USERNAME", "root");
   DEFINE("PASSWORD", "D132263c@");
