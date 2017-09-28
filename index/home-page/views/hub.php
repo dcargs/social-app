ed0e25f8e80058a1d71a87136af338ec3bfd6350
Devin Cargill <devin@Devins-MacBook-Pro.local>
2017-09-28 18:26:44 -0500

    sadf

--- index/home-page/views/hub.php
+++ index/home-page/views/hub.php
@@ -1,7 +1,17 @@
 <?php
   $title = "Your Profile";
   $onscreen_title = "My Profile";
-  $content = "Welcome";
+  session_start();
+  $alias = $_SESSION['alias'];
+  $content = "
+    <div class='jumbotron'>
+      <div class='row text-center'>
+        <h2>Welcome to your profile, ".$alias."</h2>
+      </div>
+      <hr />
+
+    </div> <!-- end jumbotron -->
+  ";
 
   require "home-page/statics/layout.php";
  ?>
