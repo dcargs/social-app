c5b0033e0d6815c76fd0603796b01a79c403eb29
Devin Cargill <devin@Devins-MacBook-Pro.local>
2017-09-28 17:14:52 -0500

    asdf

--- index/home-page/model/homeDAL.php
+++ index/home-page/model/homeDAL.php
@@ -4,23 +4,22 @@
   class homeDAL extends db {
 
     public function create_account(){
-      print_r($_POST);
-      // $alias = $_POST['alias'];
-      // $pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
-      // $f_name = ucwords($_POST['f_name']);
-      // $m_name = ucwords($_POST['m_name']);
-      // $l_name = ucwords($_POST['l_name']);
-      // $email = $_POST['email'];
-      //
-      // if($this->check_exist($alias, $email) == 1){
-      //   $stmt = $this->conn->prepare(
-      //     "INSERT INTO user (alias, pass, f_name, m_name, l_name, email) VALUES (?,?,?,?,?,?)"
-      //   );
-      //   $stmt->bind_param("ssssss", $alias, $pass, $f_name, $m_name, $l_name, $email);
-      //   $stmt->execute();
-      // } else {
-      //   echo "alias / email already in use";
-      // }
+      $alias = $_POST['alias'];
+      $pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+      $f_name = ucwords($_POST['f_name']);
+      $m_name = ucwords($_POST['m_name']);
+      $l_name = ucwords($_POST['l_name']);
+      $email = $_POST['email'];
+
+      if($this->check_exist($alias, $email) == 1){
+        $stmt = $this->conn->prepare(
+          "INSERT INTO user (alias, pass, f_name, m_name, l_name, email) VALUES (?,?,?,?,?,?)"
+        );
+        $stmt->bind_param("ssssss", $alias, $pass, $f_name, $m_name, $l_name, $email);
+        $stmt->execute();
+      } else {
+        echo "alias / email already in use";
+      }
     }
 
     private function check_exist($alias, $email){
