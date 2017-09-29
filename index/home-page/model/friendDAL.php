ff06407b3c5f649d625344581666f4151a12fba4
Devin Cargill <devin@Devins-MacBook-Pro.local>
2017-09-29 13:39:54 -0500

    asdf

--- index/home-page/model/friendDAL.php
+++ index/home-page/model/friendDAL.php
@@ -9,9 +9,9 @@
       $me = $_SESSION['alias'];
       $stmt = $this->conn->prepare(
         "SELECT alias AS Alias, f_name AS 'First Name', m_name AS 'Middle Name', l_name AS 'Last Name' FROM user
-        WHERE alias NOT LIKE ?"
+        WHERE alias NOT LIKE ? AND alias NOT IN (SELECT user1, user2 FROM friends WHERE user1 = ? OR user2 = ?)"
       );
-      $stmt->bind_param("s", $me);
+      $stmt->bind_param("sss", $me, $me, $me);
       $stmt->execute();
       $result = $stmt->get_result();
       return $result;
@@ -19,10 +19,19 @@
 
     public function send_request($me, $alias){
       $stmt = $this->conn->prepare(
-        "INSERT INTO friends (user1, user2, accepted) VALUES (?,?,0)"
+        "SELECT * FROM friends WHERE (user1 = ? OR user2 = ?) AND (user1 = ? OR user2 = ?) AND (accepted = 0)"
       );
-      $stmt->bind_param("ss", $me, $alias);
+      $stmt->bind_param("ssss", $alias, $alias, $me, $me);
       $stmt->execute();
+      $result = $stmt->get_result();
+
+      if($result->num_rows < 1){
+        $stmt = $this->conn->prepare(
+          "INSERT INTO friends (user1, user2, accepted) VALUES (?,?,0)"
+        );
+        $stmt->bind_param("ss", $me, $alias);
+        $stmt->execute();
+      }
     }
 
     public function get_requests($me){
