22c0621307eda9a55eb040d2d9626001e858eb2e
Devin Cargill <devin@Devins-MacBook-Pro.local>
2017-09-29 12:21:22 -0500

    asdf

--- index/home-page/model/friendDAL.php
+++ index/home-page/model/friendDAL.php
@@ -5,9 +5,12 @@
    */
   class friendDAL extends db {
     public function get_everyone(){
+      $me = $_SESSION['alias'];
       $stmt = $this->conn->prepare(
-        "SELECT alias AS Alias, f_name AS 'First Name', m_name AS 'Middle Name', l_name AS 'Last Name' FROM user"
+        "SELECT alias AS Alias, f_name AS 'First Name', m_name AS 'Middle Name', l_name AS 'Last Name' FROM user
+        WHERE alias NOT LIKE ?"
       );
+      $stmt->bind_param("s", $me);
       $stmt->execute();
       $result = $stmt->get_result();
       return $result;
