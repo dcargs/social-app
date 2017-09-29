2470392f5a552e54a1ee87777c45b7f6af1b997b
Devin Cargill <devin@Devins-MacBook-Pro.local>
2017-09-29 12:53:16 -0500

    asdf

--- index/home-page/views/friends.php
+++ index/home-page/views/friends.php
@@ -5,7 +5,7 @@
                    <li class='active'><a href='friends'>Friends</a></li>
                    <li><a href='messages'>Messages</a></li>";
 
-  $people = "<table style='overflow: auto;'class='table rounded table-striped table-responsive table-bordered'><thead>";
+  $people = "<span class='label label-primary'>List of Users</span><table style='overflow: auto;'class='table rounded table-striped table-responsive table-bordered'><thead>";
   while($fieldName = mysqli_fetch_field($everyone)) {
     $people .= "<th class='text-center'>" . $fieldName->name . "</th>";
   }
@@ -22,16 +22,17 @@
   }
   $people .= "</tbody></table>";
 
-  $friend_requests = "<div class='well'>";
+  $friend_requests = "<div class='well'><span class='label label-primary'>Friend Requests</span>";
   while($row = $requests->fetch_assoc()){
     $friend_requests .= "<div class='row'>";
     $friend_requests .= "<p>".$row['user2']." sent you a friend request.</p>";
     $friend_requests .= "<button type='button' id='".$row['id']."' class='btn btn-primary btn-block' onclick='respondRequest(".$row['id'].")'>Accept</button>";
     $friend_requests .= "</div><hr />";
   }
-  $friend_requests .= "</div>";
+  $friend_requests .= "</div><hr />";
 
   $content = "<div class='jumbotron'>
+                ".$friend_requests."
                 ".$people."
               </div>";
 
