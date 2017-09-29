ff0e2d3cb465b29db35678005c098cf41ae810e9
Devin Cargill <devin@Devins-MacBook-Pro.local>
2017-09-29 12:00:03 -0500

    asdf

--- index/home-page/views/friends.php
+++ index/home-page/views/friends.php
@@ -3,8 +3,27 @@
   $onscreen_title = "Connect with your friends";
   $navbar_items = "<li><a href='hub'>Profile</a></li>
                    <li class='active'><a href='friends'>Friends</a></li>
-                   <li><a href='messages'>Messages</a></li>";
-  $content = "friends<br />" . $people;
+                   <li><a href='messages'>Messages</a></li>"
+
+  $people = "<table style='overflow: auto;'class='table rounded table-striped table-responsive table-bordered'><thead>";
+  while($fieldName = mysqli_fetch_field($result)) {
+    $people .= "<th class='text-center'>" . $fieldName->name . "</th>";
+  }
+  $people .= "<th colspan='2' class='text-center'>Options</th>";
+  $people .= "</thead><tbody>";
+  while($row = $result->fetch_array(MYSQLI_NUM)) {
+    $people .= "<tr>";
+      foreach ($row as $i => $value) {
+        $people .= "<td>".$value."</td>";
+      }
+    $people .= "</tr>";
+  }
+
+  $content = "<div class='jumbotron'>
+                ".$people."
+              </div>"
+
+
 
   require "home-page/statics/layout.php";
  ?>
