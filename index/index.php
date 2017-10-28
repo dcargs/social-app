34f0b6052aafe39057a967f5ef75f37a15bca86f
Devin Cargill <devin@Devins-MacBook-Pro.local>
2017-10-28 13:26:30 -0500

    asdf

--- index/index.php
+++ index/index.php
@@ -8,10 +8,7 @@ if (!isset($_GET['key1'])) {
     if($req1 == "home") {
       require "home-page/router.php";
       route($_GET['key2']);
-    } elseif($req1 == "hub"){
-      require "hub/router.php";
-      route($_GET['key2']);
-    }
+    } 
     else {
       echo "Where are you going?";
     }
